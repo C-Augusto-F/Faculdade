425950051d384d08c31e77b994c3d25232abe719
C-Augusto-F <146351011+C-Augusto-F@users.noreply.github.com>
2024-09-11 23:31:02 -0300

    Conversor de moedas

---
+++ ConversorMoedas/CurrencyConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+public class CurrencyConverter
+{
+    private static readonly string API_URL = "https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL";
+
+    public static async Task<double> ConvertToUSD(double amountBRL)
+    {
+        var rates = await FetchExchangeRates();
+        double usdRate = rates["USDBRL"]["bid"].Value<double>();
+        return amountBRL / usdRate;
+    }
+
+    public static async Task<double> ConvertToEUR(double amountBRL)
+    {
+        var rates = await FetchExchangeRates();
+        double eurRate = rates["EURBRL"]["bid"].Value<double>();
+        return amountBRL / eurRate;
+    }
+
+    private static async Task<JObject> FetchExchangeRates()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string response = await client.GetStringAsync(API_URL);
+            return JObject.Parse(response);
+        }
+    }
+}
+
---
+++ ConversorMoedas/Form1.Designer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Digite o valor em reais para conversão:");
+        double amountBRL = Convert.ToDouble(Console.ReadLine());
+
+        try
+        {
+            double amountUSD = await CurrencyConverter.ConvertToUSD(amountBRL);
+            double amountEUR = await CurrencyConverter.ConvertToEUR(amountBRL);
+
+            Console.WriteLine($"R${amountBRL} é aproximadamente ${amountUSD} USD");
+            Console.WriteLine($"R${amountBRL} é aproximadamente €{amountEUR} EUR");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro: {ex.Message}");
+        }
+    }
+}
